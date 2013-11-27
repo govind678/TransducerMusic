5a4b3263868523b1ddbff490b875d49598325a61
Govinda Ram Pingali <govind678@gmail.com>
2013-11-26 23:33:05 -0500

    Cleaning up

--- Java/Drop1Control.java
+++ Java/Drop1Control.java
@@ -32,40 +32,70 @@ public class Drop1Control extends MaxObject {
 			processBar(bar);
 		}
 			
-		if (currentRhythm[beat-1] == 1) {
+		
+		switch (currentRhythm[beat-1]) {
+		
+		case 1:
 			outlet(0,"bang");
-		} else if (currentRhythm[beat-1] == 2) {
+			break;
+			
+		case 2:
 			outlet(1,"bang");
-		} else if (currentRhythm[beat-1] == 3) {
+			break;
+			
+		case 3:
 			outlet(2,"bang");
-		} else if (currentRhythm[beat-1] == 4) {
+			break;
+			
+		case 4:
 			outlet(3,"bang");
-		} else if (currentRhythm[beat-1] == 5) {
+			break;
+			
+		case 5:
 			outlet(4,"bang");
-		} else if (currentRhythm[beat-1] == 6) {
+			break;
+			
+		case 6:
 			outlet(5,"bang");
-		} else if (currentRhythm[beat-1] == 7) {
+			break;
+		
+		case 7:
 			outlet(6,"bang");
-		} else if (currentRhythm[beat-1] == 8) {
+			break;
+		
+		case 8:
 			outlet(7,"bang");
-		} else if (currentRhythm[beat-1] == 9) {
+			break;
+		
+		case 9:
 			outlet(1,"bang");
 			outlet(3,"bang");
-		} else if (currentRhythm[beat-1] == 10) {
+			break;
+		
+		case 10:
 			outlet(1,"bang");
 			outlet(5,"bang");
-		} else if (currentRhythm[beat-1] == 11) {
+			break;
+		
+		case 11:
 			outlet(2,"bang");
 			outlet(4,"bang");
-		} else if (currentRhythm[beat-1] == 12) {
+			break;
+		
+		case 12:
 			outlet(2,"bang");
 			outlet(6,"bang");
-		} else if (currentRhythm[beat-1] == 13) {
+			break;
+		
+		case 13:
 			outlet(5,"bang");
 			outlet(7,"bang");
-		} else if (currentRhythm[beat-1] == 14) {
+			break;
+		
+		case 14:
 			outlet(0,"bang");
 			outlet(3,"bang");
+			break;
 		}
 		
 			
